c3747df7169055c8dcc0ef07b4581602ec695b33
Renato Moraes <renato.moraes1@outlook.com>
2020-05-19 11:18:17 -0300

    EstiloController iniciado (cadastro e buscar por todos)

---
+++ src/main/java/br/com/cervejaria/controller/EstiloController.java
@@ -0,0 +1,36 @@
+package br.com.cervejaria.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cervejaria.estilo.Estilo;
+import br.com.cervejaria.repository.EstiloRepository;
+
+@RestController
+@RequestMapping("/")
+public class EstiloController {
+
+	@Autowired
+	private EstiloRepository repo;
+	
+	
+	@PostMapping("/estilos")
+	public void cadastro (@RequestBody Estilo estilo) {
+		repo.save(estilo);
+	}
+	
+	@GetMapping("/estilos")
+	public List<Estilo> getEstilo(){
+		return repo.findAll();
+		
+	}
+	
+	
+	
+}
