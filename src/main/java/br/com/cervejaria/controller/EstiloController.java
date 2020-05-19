d7e3fb282795910d8140c985e714963e70255955
Renato Moraes <renato.moraes1@outlook.com>
2020-05-19 15:34:54 -0300

    EstiloController concluído

--- src/main/java/br/com/cervejaria/controller/EstiloController.java
+++ src/main/java/br/com/cervejaria/controller/EstiloController.java
@@ -1,14 +1,19 @@
 package br.com.cervejaria.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cervejaria.cliente.Cidade;
 import br.com.cervejaria.estilo.Estilo;
 import br.com.cervejaria.repository.EstiloRepository;
 
@@ -31,6 +36,22 @@ public class EstiloController {
 		
 	}
 	
+	@GetMapping("/estilos/{id}")
+	public Estilo listId(@PathVariable("id")Long id) {
+		return repo.findById(id).get();
+		
+	}
 	
+	@PutMapping("/estilos/{id}")
+	public void update(@PathVariable("id") Long id, @RequestBody Estilo estilo) {
+		Optional<Estilo> Estilo = repo.findById(id);
+		estilo.setId(id);
+		repo.save(estilo);
+	}
+	
+	@DeleteMapping("estilos/{id}")
+	public void deletar(@PathVariable("id") Long id, @RequestBody Estilo estilo) {
+		repo.deleteById(id);
+	}
 	
 }
